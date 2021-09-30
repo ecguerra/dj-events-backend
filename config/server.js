10e9be0bf54cf803c373d8abcfb5f6b5db76158e
Elizabeth Guerra <ecguerra@outlook.com>
2021-09-30 17:50:57 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2d9670539c6dfd2477874da551c4330d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '558931cb-806e-4cbd-bae3-31af456e171a'
+};
